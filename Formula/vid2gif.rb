8932738625b144ada17ab69a401bbf29ceed8aaf
Mujasoft <20296472+mujasoft@users.noreply.github.com>
2025-07-24 09:28:31 -0400

    Minor improvements in help text release 1.0.1

--- Formula/vid2gif.rb
+++ Formula/vid2gif.rb
@@ -1,8 +1,8 @@
 class Vid2gif < Formula
   desc "Convert videos to optimized GIFs using ffmpeg"
   homepage "https://github.com/mujasoft/vid2gif"
-  url "https://github.com/mujasoft/vid2gif/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "380bbc55b0fcf12b6a752ab173226aae9559c05f722212f4c91258db21828392"
+  url "https://github.com/mujasoft/vid2gif/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "b48f6b71f1b8d698e25998dd8c5df8af093d63c2e067c4d1ccd9e360156a75da"
   license "MIT"
 
   depends_on "ffmpeg"
