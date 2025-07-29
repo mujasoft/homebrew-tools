8028a3830bc86835c0cf0ef19f2ffbc2aee0ce27
mujasoft <20296472+mujasoft@users.noreply.github.com>
2025-07-28 23:21:08 -0400

    Added gif4docs.rb

---
+++ Formula/gif4docs.rb
@@ -0,0 +1,15 @@
+class Gif4Docs < Formula
+  desc "Convert video files into optimized GIFs using FFmpeg"
+  homepage "https://github.com/mujasoft/gif4docs"
+  url "https://github.com/mujasoft/gif4docs/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "68047d2259d1ecc4a6092431ed484dc2d7517f172370b8f59522fadcfa074a48"
+  license "MIT"
+
+  def install
+    bin.install "gif4docs"
+  end
+
+  test do
+    system "gif4docs", "-h"
+  end
+end
